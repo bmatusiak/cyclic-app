a0d5b46ed0e986c57ed76a35ff255b0511278086
bmatusiak <bmatusiak@gmail.com>
2023-08-26 10:28:57 -0400

    update

--- src_server/DynamoDB.js
+++ src_server/DynamoDB.js
@@ -1,38 +1,32 @@
-const CyclicDb = require("@cyclic.sh/dynamodb")
-const db = CyclicDb(process.env.CYCLIC_DB || "dull-tan-marlin-yokeCyclicDB")
 
 var Gun = require('gun/gun');
 
 Gun.on('create', function lg(root) {
 	this.to.next(root);
-	
-	const COLLECTION = "gun";
 
+	const CyclicDb = require("@cyclic.sh/dynamodb")
+	const db = CyclicDb(process.env.CYCLIC_DB || "dull-tan-marlin-yokeCyclicDB")
+	const COLLECTION = "gun";
 	root.on('get', function (msg) {
 		this.to.next(msg);
 		var lex = msg.get, soul;
 		if (!lex || !(soul = lex['#'])) { return }
-		// if (!lex || !(key = lex['.'])) { return }
-		// console.log(soul);
 		db.collection(COLLECTION).get(soul).then((db_data) => {
 			if (db_data && db_data.props) {
 				var { value } = db_data.props;
 				var data = JSON.parse(value), tmp;
-
 				if (data && (tmp = lex['.']) && !Object.plain(tmp)) { // pluck!
 					data = Gun.state.ify({}, tmp, Gun.state.is(data, tmp), data[tmp], soul);
 				}
-
 				Gun.on.get.ack(msg, data);
 			}
 		});
-
 	});
 
 	root.on('put', function (msg) {
 		this.to.next(msg); // remember to call next middleware adapter
 		var put = msg.put, soul = put['#'], key = put['.'], id = msg['#'];//, ok = msg.ok||'', tmp; // pull data off wire envelope
-		
+
 		db.collection(COLLECTION).get(soul).then((db_data) => {
 			var data = {};
 			if (db_data && db_data.props) {
@@ -42,14 +36,10 @@ Gun.on('create', function lg(root) {
 
 			data = Gun.state.ify(data, key, put['>'], put[':'], soul); // merge into disk object
 
-			db.collection(COLLECTION).set(soul, { value: JSON.stringify(data) }).then(()=>{
-				root.on('in', {'@': id, err: null, ok: 0});
+			db.collection(COLLECTION).set(soul, { value: JSON.stringify(data) }).then(() => {
+				root.on('in', { '@': id, err: null, ok: 0 });
 			})
 		});
-
-		// .then((data) => {
-		// 	Gun.on.get.ack(msg, data);
-		// });
 
 	});
 });
